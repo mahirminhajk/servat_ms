bf355ad01cde0252008815045a1ccc89fe0c24e8
mahirminhajk <kmahirminhaj@gmail.com>
2024-12-13 01:26:13 +0530

    fix: client basic fix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -28,14 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
