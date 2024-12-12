31a6c46dbb5dc63db592f950c45615fc6dc8d985
mahirminhajk <kmahirminhaj@gmail.com>
2024-12-13 00:56:25 +0530

    init: admin panel with shadcn config

---
+++ admin-panel/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <p>Hello this the admin panel</p>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
